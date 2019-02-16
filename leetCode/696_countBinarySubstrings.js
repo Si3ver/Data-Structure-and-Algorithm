03a868177e3cb5137d7dc43b49d52fe18fc8bda6
Si3ver <izhouwl@163.com>
2019-02-16 20:39:38 +0800

    find an O(n) solution for 696

--- leetCode/696_countBinarySubstrings.js
+++ leetCode/696_countBinarySubstrings.js
@@ -1,5 +1,6 @@
 // 计数二进制子串
-var countBinarySubstrings = function(str) {
+// 解法1： 每次往后匹配第i个子串，如果match成功，就加一。 O(n) * O(match)
+var countBinarySubstrings1 = function(str) {
     let match = (str) => {
         let j = str.match(/^(0+|1+)/)[0]                // 前面的连续1或0
         let o = (j[0] ^ 1).toString().repeat(j.length)  // 同样长度的0或1
@@ -12,12 +13,33 @@ var countBinarySubstrings = function(str) {
     return cnt
 };
 
+// 解法2： O(n)
+var countBinarySubstrings2 = function(s) {
+    let result = 0, curLen = 1, preLen = 0
+    for (let i = 0, len = s.length - 1; i < len; i++) {
+      if (s[i] === s[i+1]) {
+        ++curLen
+      } else {
+        preLen = curLen
+        curLen = 1
+      }
+      if (preLen >= curLen) {
+        ++result
+      }
+    }
+    return result
+};
+
+
 // --- test ---
-let res = []
+let res1 = [], res2 = []
 let originStr1 = "00110011"
-res.push(countBinarySubstrings(originStr1))
+res1.push(countBinarySubstrings1(originStr1))
+res2.push(countBinarySubstrings2(originStr1))
 let originStr2 = "10101"
-res.push(countBinarySubstrings(originStr2))
+res1.push(countBinarySubstrings1(originStr2))
+res2.push(countBinarySubstrings2(originStr2))
 let originStr3 = "00110"
-res.push(countBinarySubstrings(originStr3))
-console.log(res)
+res1.push(countBinarySubstrings1(originStr3))
+res2.push(countBinarySubstrings2(originStr3))
+console.log(res1, res2)
