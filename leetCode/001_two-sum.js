6a12c6cea8781563e7b618565418e89ef1384c92
Si3ver <izhouwl@163.com>
2020-05-01 23:42:56 +0800

    two-sum

---
+++ leetCode/001_two-sum.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+// method1 bad O(N^2)
+var twoSum_bad = function(nums, target) {
+  for (let i = 0; i < nums.length - 1; ++i) {
+    for (let j = i + 1; j < nums.length; ++j) {
+      if (nums[i] + nums[j] === target) {
+        return [i , j]
+      }
+    }
+  }
+};
+
+// O(N)
+var twoSum = function (nums, target) {
+  var hashmap = new Map()
+  for (var i = 0; i < nums.length; ++i) {
+    var cur = nums[i], gap = target - cur
+    if (hashmap.get(cur) !== void (0)) {
+      return [hashmap.get(cur), i]
+    }
+    hashmap.set(gap, i)
+  }
+  return []
+};
+
+// test case
+console.log(twoSum([2, 7, 11, 5], 9))
