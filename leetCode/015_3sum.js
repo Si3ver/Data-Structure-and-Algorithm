969ad192c2334f45d761cc23e362e68491de319d
Si3ver <izhouwl@163.com>
2020-05-11 21:52:43 +0800

    stash

--- leetCode/015_3sum.js
+++ leetCode/015_3sum.js
@@ -3,8 +3,22 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
+  let res = []
+  let len = nums.length
+  nums.sort((a, b) => a - b)
+  if (nums[0] > 0 || nums[len - 1] < 0) return res
+  for (let i = 0; i < len - 2; ++i) {
+    if (nums[i] < 0) {
+      let firstIdx = i + 1
+      let lastIdx = len - 1
+      do {
+
+      }
+    }
+  }
 
 };
 
 
 // test cases
+console.log(threeSum([-1, 0, 1, 2, -1, -4]))
