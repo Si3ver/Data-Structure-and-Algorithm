08a28b1967cf762322ff633cd085a6e3b37fc06a
Si3ver <izhouwl@163.com>
2024-01-05 00:08:22 +0800

    feat: solve 153 max product with momo

---
+++ leetCode/152_maxProduct.js
@@ -0,0 +1,22 @@
+/**
+ * https://leetcode.cn/problems/maximum-product-subarray
+ * 乘极最大的子数组
+ *
+ */
+
+var maxProduct = function(A) {
+  if (!Array.isArray(A) || A.length < 1) return;
+  const n = A.length;
+  let min = max = res = A[0];
+  for (let i = 1; i < n; ++i) {
+    if (A[i] < 0) [min, max] = [max, min];
+    max = Math.max(max * A[i], A[i]);
+    min = Math.min(min * A[i], A[i]);
+    res = Math.max(res, max);
+  }
+  return res;
+};
+
+// ---- test case ----
+console.log(maxProduct([2, 3, -2, 4]));
+console.log(maxProduct([-2, -1]));
