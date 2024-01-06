9ebf8a1954506fdb0c5af0ca13e5e5a689f465d7
Si3ver <izhouwl@163.com>
2024-01-07 00:00:34 +0800

    feat: solve 064 with dp2dim

---
+++ leetCode/064_minPathSum.js
@@ -0,0 +1,32 @@
+/**
+ * https://leetcode.cn/problems/minimum-path-sum/description
+ * 最小路径和
+ *
+ * f(i, j) = A[i][j] + min(f(i - 1, j), f(i, j - 1))
+ *
+ *  1  3  1
+ *  1  5  1
+ *  4  2  1
+ *
+ *  1  4  5
+ *  2  7  6
+ *  6  8  7
+ */
+var minPathSum = function(A) {
+  const m = A.length, n = A[0].length;
+  const dp = A[0].slice();
+  for (let i = 1; i < n; ++i) {
+    dp[i] += dp[i - 1];
+  }
+
+  for (let i = 1; i < m; ++i) {
+    dp[0] += A[i][0];
+    for (let j = 1; j < n; ++j) {
+      dp[j] = Math.min(dp[j], dp[j - 1]) + A[i][j];
+    }
+  }
+  return dp[n - 1];
+};
+
+// ---- test case ----
+console.log(minPathSum([[1,3,1],[1,5,1],[4,2,1]])); // 7
