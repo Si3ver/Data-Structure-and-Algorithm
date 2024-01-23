de1751b3eb5320a24c6fa1b58ca2e86f087bc2f1
Si3ver <izhouwl@163.com>
2024-01-23 12:33:44 +0800

    feat: sliding window

---
+++ leetCode/239_maxSlidingWindow.js
@@ -0,0 +1,26 @@
+/**
+ * https://leetcode.cn/problems/sliding-window-maximum/
+ * 滑动窗口最大值
+ * hard
+ */
+
+var maxSlidingWindow = function (arr, k) {
+  const res = [], wind = [];
+  for (let i = 0; i < arr.length; ++i) {
+    if (wind.length > 0 && i - wind[0] >= k) { // delete queue left item
+      wind.shift();
+    }
+    while (wind.length > 0 && arr[i] >= arr[wind[wind.length - 1]]) { // delete queue right little old items
+      wind.pop();
+    }
+    wind.push(i);
+    if (i >= k - 1) {
+      res.push(arr[wind[0]]);
+    }
+  }
+  return res;
+};
+
+// ----
+console.log(maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3)); // [3,3,5,5,6,7]
+console.log(maxSlidingWindow([1], 1)); // [1]
