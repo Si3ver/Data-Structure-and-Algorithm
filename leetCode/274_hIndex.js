1780d97394dfc6eb7bc4d3466e7b8c8b5200dbc8
Si3ver <izhouwl@163.com>
2024-01-29 00:30:54 +0800

    feat: solve hIndex

--- leetCode/274_hIndex.js
+++ leetCode/274_hIndex.js
@@ -2,16 +2,27 @@
  * https://leetcode.cn/problems/h-index
  * h指数
  * medium
+ *
+ * 引用次数   i     0  1  2  3  4 >=5
+ * 文章数量 cntArr  1  1  0  1  0  2
  */
 
+// 计数法，O(n)
 var hIndex = function (A) {
   const n = A.length;
   const cntArr = Array(n + 1).fill(0);
   for (const num of A) {
-    ++cntArr[Math.min(num, n)];
+    ++cntArr[Math.min(num, n)]; // 引用次数统计
   }
-  // console.log(cntArr);
+  let sum = 0;
+  for (let i = n; i >= 0; --i) {
+    sum += cntArr[i];
+    if (sum >= i) {
+      return i;
+    }
+  }
 }
 
 // ----
-console.log(hIndex([3, 0, 6, 1, 5]));
+console.log(hIndex([3, 0, 6, 1, 5])); // 3
+console.log(hIndex([1, 3, 1])); // 1
