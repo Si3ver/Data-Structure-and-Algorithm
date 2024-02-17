f51d5b3a82c7ff7a16f04defd8a3d058bcc61de2
Si3ver <izhouwl@163.com>
2024-02-17 11:11:52 +0800

    feat: solve partitionLabels

--- leetCode/763_partitionLabels.js
+++ leetCode/763_partitionLabels.js
@@ -5,7 +5,21 @@
  */
 
 var partitionLabels = function (S) {
-
+  const lastPosMap = new Map();
+  for (let i = 0; i < S.length; ++i) {
+    lastPosMap.set(S[i], i);
+  }
+  const res = [];
+  let start = 0, max = 0;
+  for (let i = 0; i < S.length; ++i) {
+    const currentLastPos = lastPosMap.get(S[i]);
+    max = Math.max(max, currentLastPos);
+    if (i === max) {
+      res.push(i - start + 1);
+      start = i + 1;
+    }
+  }
+  return res;
 }
 
 // ----
