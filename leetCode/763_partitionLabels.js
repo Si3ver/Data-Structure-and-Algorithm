dd9a3a3fbfc43de6a60674c9a5da794a464ceb57
Si3ver <izhouwl@163.com>
2024-02-17 11:32:56 +0800

    feat: add some comment

--- leetCode/763_partitionLabels.js
+++ leetCode/763_partitionLabels.js
@@ -2,6 +2,8 @@
  * https://leetcode.cn/problems/partition-labels/
  * 划分字母区间
  * medium
+ *
+ * 思路：map 存储字符最后出现的下标，
  */
 
 var partitionLabels = function (S) {
@@ -9,6 +11,7 @@ var partitionLabels = function (S) {
   for (let i = 0; i < S.length; ++i) {
     lastPosMap.set(S[i], i);
   }
+  console.log(lastPosMap);
   const res = [];
   let start = 0, max = 0;
   for (let i = 0; i < S.length; ++i) {
@@ -24,4 +27,23 @@ var partitionLabels = function (S) {
 
 // ----
 console.log(partitionLabels('ababcbacadefegdehijhklij')); // [9, 7, 8]
+/*
+  a: 8
+  b: 5
+  c: 7
+  d: 14
+  e: 15
+  f: 11
+  g: 13
+  h: 19
+  i: 22
+  j: 23
+  k: 20
+  l: 21
+
+  index 0  1  2  3  4  5  6  7  8 |  9 10 11 12 13 14 15 | 16 17 18 19 20 21 22 23
+  last  8  5  8  5  7  5  8  7  8 | 14 15 11 15 13 14 15 | 19 22 23 19 20 21 22 23
+  ch    a  b  a  b  c  b  a  c  a |  d  e  f  e  g  d  e |  h  i  j  h  k  l  i  j
+                                9                      7                         8
+ */
 console.log(partitionLabels('eccbbbbdec')); // [10]
