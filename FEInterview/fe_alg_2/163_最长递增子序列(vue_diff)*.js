c837c92f8ebd902018aaf937a18d649542493d25
Si3ver <izhouwl@163.com>
2024-02-28 23:01:36 +0800

    feat: solve

---
+++ FEInterview/fe_alg_2/163_最长递增子序列(vue_diff)*.js
@@ -0,0 +1,92 @@
+/**
+ * https://leetcode.cn/problems/longest-increasing-subsequence
+ * 最长递增子序列
+ *
+ * medium
+ *
+ * 输入：[10, 9, 2, 5, 3, 7, 101, 18]
+   输出：4
+   解释：最长递增子序列是 [2, 3, 7, 101]，因此长度为 4 。
+ */
+
+/*
+  思路一： 暴力线性 DP
+  定义 f(i) 表示  i 必选时，最长递增子序列的长度
+  f(i) = max(f(j) + 1)    j
+  O(n^2) O(n)
+*/
+var lengthOfLIS = function(arr) {
+  const n = arr.length;
+  if (n < 2) return n;
+  const dp = Array(n).fill(1);
+  for (let i = 1; i < n; ++i) {
+    for (let j = 0; j < i; ++j) {
+      if (arr[j] < arr[i]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+  return Math.max(...dp);
+};
+
+
+/*
+解法二： 二分优化后的DP： O(nlogn)
+      i |   0   1   2   3   4   5   6   7
+    arr |  10   9   2   5   3   7 101  18
+     dp | 102 102 102 102 102 102 102 102
+     dp |  10
+     dp |   9
+     dp |   2
+     dp |       5
+     dp |       3
+     dp |           7
+     dp |             101
+     dp |              18
+
+      i |   0   1   2   3   4   5
+    arr |   0   1   0   3   2   3
+     dp |   ∞   ∞   ∞   ∞   ∞   ∞
+     dp |   0
+     dp |       1
+     dp |   0
+     dp |           3
+     dp |           2
+     dp |               3
+*/
+var lengthOfLIS = function(arr) {
+  const n = arr.length;
+  if (n < 2) return n;
+
+  const search = (dp, target, hi) => {
+    let lo = 0;
+    while (lo <= hi) {
+      let mid = lo + ((hi - lo) >> 1);
+      if (target === dp[mid]) {
+        return mid;
+      } else if (target < dp[mid]) {
+        hi = mid - 1;
+      } else {
+        lo = mid + 1;
+      }
+    }
+    return lo;
+  }
+
+  const UPBOUND = Math.max(...arr) + 1;
+  const dp = Array(n).fill(UPBOUND);
+  for (let i = 0; i < n; ++i) {
+    let pos = search(dp, arr[i], i);
+    dp[pos] = arr[i];
+    // console.log(pos, dp);
+  }
+  for (let i = dp.length - 1; i >= 0; --i) {
+    if (dp[i] !== UPBOUND) return i + 1;
+  }
+  return 0; // 👈 这句应该永远执行不到
+};
+
+// ---- test case ----
+// console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18])) // 4
+console.log(lengthOfLIS([0, 1, 0, 3, 2, 3])) // 4
+// console.log(lengthOfLIS([7, 7, 7, 7, 7, 7, 7])) // 1
---
+++ FEInterview/fe_alg_2/170_最长回文子串.js
@@ -0,0 +1,38 @@
+/**
+ * https://leetcode.cn/problems/longest-palindromic-substring
+ * 005.最长回文串
+ * medium
+ *
+ * 求一个字符串的最长回文子串，例如"212332145"的最长回文子串是"123321"
+ * 思路：中心扩散法O(n^2)
+ */
+
+var longestPalindrome = function(s) {
+  const n = s.length;
+  if (n < 2) return s;
+
+  const expand = (lo, hi = lo) => {
+    while (lo >= 0 && hi < n && s[lo] === s[hi]) {
+      --lo;
+      ++hi;
+    }
+    return hi - lo - 1;
+  };
+
+  let start = 0, end = 0;
+  for (let i = 0; i < n; ++i) {
+    const lenOdd = expand(i);
+    const lenEven = expand(i, i + 1);
+    const len = Math.max(lenOdd, lenEven);
+    if (len > end - start + 1) {
+      end = i + (len >> 1); // (lenOdd - 1) / 2 or lenEven / 2
+      start = i - ((len - 1) >> 1); // (lenOdd - 1) / 2 or (lenEven - 2) / 2
+    }
+  }
+  return s.slice(start, end + 1);
+};
+
+// ---- test cases ----
+console.log(longestPalindrome('212332145')); // 123321
+console.log(longestPalindrome('babad')); // 'bab'
+console.log(longestPalindrome('cbbd')); // 'bb'
