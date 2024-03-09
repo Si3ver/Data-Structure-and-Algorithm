f1d63a48cdc937e75afc91b50d7ce0e7e5d67c90
Si3ver <izhouwl@163.com>
2024-03-09 10:29:21 +0800

    feat: ts trick 2 string & index type

--- FEInterview/01_typescript/guang-ts/06_trick2.ts
+++ FEInterview/01_typescript/guang-ts/06_trick2.ts
@@ -37,8 +37,39 @@
 }
 
 // ------------------------------ 2. 字符串 ------------------------------
+// 内置高级类型: Uppercase、 Lowercase、 Capitalize、 Uncapitalize
 {
+  type CapitalizeStr<Str extends string> =
+    Str extends `${infer First}${infer Rest}`
+      ? `${Uppercase<First>}${Rest}`
+      : Str;
 
+  // test
+  type res1 = CapitalizeStr<'typescript'>;
+}
+
+{
+  //❎ ? `${Capitalize<Left>}${CamelCase<Rest>}`
+  // 递归调用
+  type CamelCase<Str extends string> =
+    Str extends `${infer Left}_${infer Right}${infer Rest}`
+      ? `${Left}${Uppercase<Right>}${CamelCase<Rest>}`
+      : Str;
+
+  // test
+  type res1 = CamelCase<'aaa_bbb_ccc'>; // expect => aaaBbbCcc
+}
+
+{
+  type DropSubStr<Str extends string, SubStr extends string> =
+    Str extends `${infer Prefix}${SubStr}${infer Suffix}`
+      ? DropSubStr<`${Prefix}${Suffix}`, SubStr>
+      : Str;
+
+  // test
+  type res1 = DropSubStr<'TypeScript~~~', '~'>;
+  type res2 = DropSubStr<'TypeScript~~~JavaScript', '~'>;
+  type res3 = DropSubStr<'T～ype~~S  cript', '~'>;
 }
 
 // ------------------------------ 3. 函数 ------------------------------
@@ -48,5 +79,30 @@
 
 // ------------------------------ 4. 索引类型 ------------------------------
 {
+  // 通用写法（公式） -- as
+  type Mapping<Obj extends object> = {
+    [Key in keyof Obj]: Obj[Key];
+  };
 
+  // test
+  type User = {
+    readonly id: string;
+    name: string;
+    age?: number;
+  };
+  type res1 = Mapping<User>;
+}
+
+{
+  type UppercaseKey<Obj extends Record<string, any>> = {
+    [Key in keyof Obj as Uppercase<Key & string>]: Obj[Key];
+  }
+
+  // test
+  type User = {
+    id: string;
+    name: string;
+    age: number;
+  };
+  type res1 = UppercaseKey<User>;
 }
