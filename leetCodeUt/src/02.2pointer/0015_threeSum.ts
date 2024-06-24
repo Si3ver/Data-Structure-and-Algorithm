407b3d3164f47de9c111cf96cbd9785ad0f9e298
Si3ver <izhouwl@163.com>
2024-06-24 23:21:43 +0800

    feat: 3sum

--- leetCodeUt/src/02.2pointer/0015_threeSum.ts
+++ leetCodeUt/src/02.2pointer/0015_threeSum.ts
@@ -1,18 +1,21 @@
-
-
 // 思路：先排序，然后双指针夹逼
 function threeSum(nums: number[]): number[][] {
   const res: number[][] = [];
   nums.sort((x, y) => x - y);
   for (let i = 0; i < nums.length - 2 && nums[i] <= 0; ++i) {
     if (i === 0 || nums[i] !== nums[i - 1]) {
-      let l = i + 1, r = nums.length - 1;
+      let l = i + 1,
+        r = nums.length - 1;
       while (l < r) {
         const sum = nums[i] + nums[l] + nums[r];
         if (sum === 0) {
           res.push([nums[i], nums[l++], nums[r--]]);
-          while (l < r && nums[l] === nums[l-1])  ++l; // 去重
-          while (l < r && nums[r] === nums[r+1])  --r; // 去重
+          while (l < r && nums[l] === nums[l - 1]) {
+            ++l;
+          }
+          while (l < r && nums[r] === nums[r + 1]) {
+            --r;
+          }
         } else if (sum > 0) {
           --r;
         } else {
@@ -22,6 +25,6 @@ function threeSum(nums: number[]): number[][] {
     }
   }
   return res;
-};
+}
 
-export {threeSum}
+export { threeSum };
