05621bb70a55eded762eddea760cff8cf6412272
Si3ver <izhouwl@163.com>
2024-09-01 23:02:09 +0800

    feat: reverseList

---
+++ leetCodeUt/src/07.linkedList/0206_reverseList.ts
@@ -0,0 +1,10 @@
+function reverseList(head: ListNode | null): ListNode | null {
+  let prev = null;
+  while (head) {
+    const next = head.next;
+    head.next = prev;
+    prev = head;
+    head = next;
+  }
+  return prev;
+}
